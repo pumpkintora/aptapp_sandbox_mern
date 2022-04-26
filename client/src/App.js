b32afb178250d1321a1bb0a45b4aa57512474097
rk <renkiat.gan@corpxervices.com>
2022-04-26 18:35:08 +0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Homepage from "./homepage"
+import Login from "./login"
+import Register from "./register"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import { useState } from 'react';
+
+function App() {
+  const [user, setLoginUser] = useState({})
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            {user && user._id ? <Homepage /> : <Login />}<Homepage />
+          </Route>
+          <Route path="/Login"><Login setLoginUser={setLoginUser} /></Route>
+          <Route path="/Register"><Register /></Route>
+        </Routes>
+
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
