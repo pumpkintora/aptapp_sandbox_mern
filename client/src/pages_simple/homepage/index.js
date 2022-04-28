d5cc3e7f0f26a938711a4e4b7a49bf714f8e687e
rk <renkiat.gan@corpxervices.com>
2022-04-28 16:00:21 +0800

    remove token from homepagte

--- client/src/pages_simple/homepage/index.js
+++ client/src/pages_simple/homepage/index.js
@@ -10,7 +10,7 @@ const Homepage = () => {
     React.useEffect(() => {
         // console.log(axios.defaults.headers.common["Authorization"])
         axios.post('http://localhost:6969/token/verify', {
-            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmEwYmZhODVmMGMyY2Y5NTVjZmM4NyIsImVtYWlsIjoiZ3JrQGdyay5jb20iLCJpYXQiOjE2NTExMzIyNDksImV4cCI6MTY1MTEzMjI1OX0.k_CBwAAqIwmw8MhuW4I3kNXLxr7ACfN1T5-cfsBKSX5"
+            token: axios.defaults.headers.common["Authorization"]
         }).then((res) => {
             console.log(res)
             if (res.status === 400) navigate('/login')
